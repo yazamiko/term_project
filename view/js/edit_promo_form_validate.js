8d4d53f968f667825877136df4ccbb3f562b409a
bigdhall34 <bigdhall34@gmail.com>
2015-11-17 17:54:39 -0500

    Update Promotion Redirect

--- view/js/edit_promo_form_validate.js
+++ view/js/edit_promo_form_validate.js
@@ -27,7 +27,7 @@ function getSubmitStatus(response) {
 
    	if(arr.status) {
    		$( "#modalButton" ).click(function() {
-  			location.reload();
+  			 location.href = "edit_promo_form_ui.html?promo_code=" + promoCode;
 		});
    	}
 }
