b5f53ac19f5b145e2f6a76278f5146f5ac0a320f
unknown <bigdhall34@gmail.com>
2015-12-09 11:29:49 -0500

    ItemPromo for Report 3

---
+++ model/itemPromo.php
@@ -0,0 +1,68 @@
+<?php
+	//Entity ItemPromo
+	class itemPromo {
+		//class attr
+		private $promoCode;
+		private $itemNumber;
+		private $itemDescription;
+		private $fullRetailPrice;
+		private $salePrice;
+
+		//construct
+		public function __construct() {
+
+		}
+
+		//setters and getters
+		/*--------------------
+			Promo Code
+		 ---------------------*/
+		public function setPromoCode($promoCode) {
+			$this->promoCode = $promoCode;
+		}
+
+		public function getPromoCode() {
+			return $this->promoCode;
+		}
+		/*--------------------
+			Item Number
+		 ---------------------*/
+		public function setItemNumber($itemNumber) {
+			$this->itemNumber = $itemNumber;
+		}
+
+		public function getItemNumber() {
+			return $this->itemNumber;
+		}
+		/*--------------------
+		    Item Description
+		 ---------------------*/
+		public function setItemNumber($itemDescription) {
+			$this->itemDescription = $itemDescription;
+		}
+
+		public function getItemDescription() {
+			return $this->itemDescription;
+		}
+		/*--------------------
+		Item Full Retail Price
+		 ---------------------*/
+		public function setFullRetailPrice($fullRetailPrice) {
+			$this->fullRetailPrice = $fullRetailPrice;
+		}
+
+		public function getFullRetailPrice() {
+			return $this->fullRetailPrice;
+		}
+		/*--------------------
+			Sale Price
+		 ---------------------*/
+		public function setSalePrice($salePrice) {
+			$this->salePrice = $salePrice;
+		}
+
+		public function getSalePrice() {
+			return $this->salePrice;
+		}
+	}
+?>
