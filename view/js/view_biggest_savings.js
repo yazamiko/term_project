d1971a2ac3f78cc0b4e851d7ac7b5e2868976d18
yazamiko <yazamiko@gmail.com>
2015-12-09 14:51:03 -0500

    few changes

--- view/js/view_biggest_savings.js
+++ view/js/view_biggest_savings.js
@@ -45,7 +45,7 @@ function retrievePromotion() {
     
     xmlhttp.onreadystatechange=function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-		alert(xmlhttp.responseText);
+		  //alert(xmlhttp.responseText);
             preparePromotionResult(xmlhttp.responseText);
         }
     }
